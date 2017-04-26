0ef5cf18b2825150c48d82a081113ec5ab6f315f
Andrei Koenig <nv1982@gmail.com>
2017-04-25 20:53:55 -0400

    10.4.2 wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
-
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
@@ -12,6 +8,9 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   resources :users
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:andrei)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -13,4 +17,17 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select "title", full_title("Sign up")
   end
+
+  test "should redirect to login page when accessing user index if not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
+  test "should display user index when logged in" do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+  end
+
+
 end
